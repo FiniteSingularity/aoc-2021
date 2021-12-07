09400992c3893e6509f133217b51bd934eacd31e
Finite Singularity <finitesingularityttv@gmail.com>
2021-12-07 22:38:44 +0000

    Day 7

---
+++ 07/script_1.py
@@ -0,0 +1,29 @@
+import statistics
+import math
+
+with open('./input', encoding='utf8') as file:
+    positions = [int(value) for value in file.readline().split(',')]
+
+max_position = max(positions)
+min_position = min(positions)
+mean_position = statistics.mean(positions)
+if (mean_position - min_position) < (max_position - mean_position):
+    rng = range(min_position, math.ceil(mean_position)+1)
+else:
+    rng = range(math.floor(mean_position), max_position)
+
+min_fuel = 10000000
+
+for alignment in rng:
+    fuel = [abs(position-alignment) for position in positions]
+    total_fuel = sum(fuel)
+    min_fuel = min(min_fuel, total_fuel)
+
+print(min_fuel)
+
+median_position = statistics.median(positions)
+alignment = median_position
+fuel = [abs(position-alignment) for position in positions]
+total_fuel = sum(fuel)
+
+print(total_fuel)
